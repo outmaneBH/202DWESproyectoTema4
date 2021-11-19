dd4b81a933265c616dcc7fca6534191894fdc076
outmane <outmane.bou.1@educa.jcyl.es>
2021-11-19 13:55:04 +0100

    19/11/2021 Añadir ficheros de 1&1 de configuracion.

--- codigoPHP/ejercicio2.0.php
+++ codigoPHP/ejercicio2.0.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="UTF-8">
         <title>Ejercicio2 - PDO</title>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <style>
